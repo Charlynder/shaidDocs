d1977681f6b3cfff5403b3f0c747140fb7db8725
Christian Alvarez <alvarez.christian46@gmail.com>
2025-01-19 17:13:50 -0600

    feat: settingsScreen.dart added

--- docs/lib/screens/editorScreen.dart
+++ docs/lib/screens/editorScreen.dart
@@ -1,9 +1,7 @@
 // the editor screen is where the user can edit the text of the page or pages
 // it will be the main screen that the user will interact with
 // the user will be able to edit documents, save them, and share them within this screen
-// it will use a options menu bar to allow the user to make changes to the text of the document, like bold,
-// italic, underline, etc. using the rich text editor with the option to use markdown like syntax as a way to edit
-// the text
+// it will use a options menu bar to allow the user to make changes to the text of the document, like bold, italic, underline, etc. using the rich text editor with the option to use markdown as a way the 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -16,8 +14,34 @@ class EditorScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Editor Screen'),
       ),
-      body: const Center(
-        child: Text('Welcome to the Editor Screen!'),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            // 30% from the top
+            margin: const EdgeInsets.only(top: 30.0), 
+            // A4 width, height in pixels at 72 DPI
+            width: 595, 
+            height: 842,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  // changes position of shadow
+                  offset: Offset(0, 3), 
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Container(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
--- docs/lib/screens/homeScreen.dart
+++ docs/lib/screens/homeScreen.dart
@@ -9,23 +9,49 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Welcome to the Home Screen!'),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/editor');
-              },
-              child: const Text('Go to Editor Screen'),
+      body: Row(
+        children: [
+          Container(
+            width: 250, // Set the width of the side column
+            color: Colors.white,
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                ListTile(
+                  leading: const Icon(Icons.settings),
+                  title: const Text('Settings'),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/settings');
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.edit),
+                  title: const Text('Editor'),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/editor');
+                  },
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Welcome to the Home Screen!'),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/editor');
+                    },
+                    child: const Text('Go to Editor Screen'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
